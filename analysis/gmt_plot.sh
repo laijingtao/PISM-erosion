17b7960ea3536d2a96e7be87c5b6a86f4813299b
laijingtao <laijingtao@gmail.com>
2017-05-04 15:43:12 -0500

    add support for smaller domain

--- analysis/gmt_plot.sh
+++ analysis/gmt_plot.sh
@@ -8,7 +8,8 @@ var=thk
 cbartitle=Thickness
 cbarunit=m
 cbar=0/2000/100
-range=-124.8/-122.6/47/48.4
+#range=-124.8/-122.6/47/48.4
+range=-124.5/-122.75/47.25/48.15
 
 # set up the option parser
 while [[ $# -gt 1 ]]
@@ -50,10 +51,11 @@ python ../tools/nc2gmt.py -i $infile -o tmp.xyz -v $var --srs "+init=epsg:26710"
 
 gmt xyz2grd tmp.xyz -Gtmp.nc -R$range -I0.5m
 
-gmt psbasemap -R$range -Jm6 -Ba1f0.25 -V -P -K -X1.5 -Y2 >> tmp.ps 
+gmt psbasemap -R$range -Jm6 -Ba0.5f0.25 -V -P -K -X1.5 -Y2 >> tmp.ps 
 gmt makecpt -T$cbar -Cjet -Z >tmp.cpt
 gmt grdimage tmp.nc -Ctmp.cpt -Jm -E300 -nb -Q -P -O -K >> tmp.ps
-gmt psscale -Dx15.5c/0.3c+w12c/0.5c -Ctmp.cpt -Baf -Bx+l$cbartitle -By+l$cbarunit -O >> tmp.ps
+#gmt psscale -Dx15.5c/0.3c+w12c/0.5c -Ctmp.cpt -Baf -Bx+l$cbartitle -By+l$cbarunit -O >> tmp.ps
+gmt psscale -Dx12.5c/0.11c+w8c/0.4c -Ctmp.cpt -Baf -Bx+l$cbartitle -By+l$cbarunit -O >> tmp.ps
 
 #ps2pdf tmp.ps $outfile
 gmt psconvert tmp.ps -A -Tj
