405cbed1793091eebc9dbffc75d75a454f873763
laijingtao <laijingtao@gmail.com>
2017-10-14 16:22:26 -0500

    built_climate_file can take args

--- data_sets/climate_forcing/build_climate_file.py
+++ data_sets/climate_forcing/build_climate_file.py
@@ -3,60 +3,97 @@
 
 import numpy as np
 from netCDF4 import Dataset
+import subprocess as sub
 
-infile = '../bed_dem/pism_Synthetic_v1.nc'
-outfile = 'constant_climate.nc'
+def build_paleo_modifier(delta_T=[0.0], frac_P=[1.0], *args, **kwargs):
+    cmd = ['/usr/bin/ncgen', '-b', '-o', 'paleo_modifier.nc', 'paleo_modifier.cdl']
+    print ' '.join(cmd)
+    sub.call(cmd)
+    for P in frac_P:
+        for T in delta_T:
+            cmd = ['ncap2', '-O', '-s', 'delta_T(0)={};frac_P(0)={}'.format(T, P),
+                   'paleo_modifier.nc', 'paleo_modifier_T_{}_P_{}.nc'.format(T, P)]
+            print ' '.join(cmd)
+            sub.call(cmd)
 
-# dimensions of Olympic
-x_dim = 200
-y_dim = 180
-dim_value = {'x': np.arange(x_dim),
-             'y': np.arange(y_dim)}
-if not(infile is None):
-    indata = Dataset(infile, 'r')
-    x_dim, = indata.variables['x'].shape
-    y_dim, = indata.variables['y'].shape
-    dim_value['x'] = indata.variables['x'][:]
-    dim_value['y'] = indata.variables['y'][:]
-    indata.close()
-
-air_temp_mean_annual = 9.5-2
-air_temp_mean_july = 15.5-2
-precipitation = 2000.0
-usurf = 0.0
-
-outdata = Dataset(outfile, 'w')
-
-dim_len = {'x': x_dim, 'y': y_dim, 'time': None}
-for dim_name in ['x', 'y', 'time']:
+def build_constant_climate(infile=None, outfile=None, *args, **kwargs):
     try:
-        outdata.createDimension(dim_name, dim_len[dim_name])
-        outdata.createVariable(dim_name, np.float64, (dim_name,))
-        if not(dim_len[dim_name] is None):
-            outdata.variables[dim_name][:] = dim_value[dim_name]
+        air_temp_mean_annual = kwargs['air_temp_mean_annual']
     except:
-        continue
-
-var_name_list = ['air_temp_mean_annual',
-                 'air_temp_mean_july',
-                 'precipitation',
-                 'usurf']
-var_value = {'air_temp_mean_annual': air_temp_mean_annual,
-             'air_temp_mean_july': air_temp_mean_july,
-             'precipitation': precipitation,
-             'usurf': usurf}
-var_unit = {'air_temp_mean_annual': 'celsius',
-             'air_temp_mean_july': 'celsius',
-             'precipitation': 'mm yr-1',
-             'usurf': 'm'}
-for var_name in var_name_list:
+        air_temp_mean_annual = 9.5-2
     try:
-        var = outdata.variables[var_name]
+        air_temp_mean_july = kwargs['air_temp_mean_july']
     except:
-        var = outdata.createVariable(var_name, np.float64, ('y', 'x',))
-    var[:] = var_value[var_name]
-    var.units = var_unit[var_name]
+        air_temp_mean_july = 15.5-2
+    try:
+        precipitation = kwargs['precipitation']
+    except:
+        precipitation = 2000.0
+    try:
+        usurf = kwargs['usurf']
+    except:
+        usurf = 0.0
+    
+    write_data(infile, outfile, air_temp_mean_annual=air_temp_mean_annual,
+               air_temp_mean_july=air_temp_mean_july, precipitation=precipitation,
+               usurf=usurf)
 
-outdata.variables['usurf'].standard_name = 'surface_altitude'
+def write_data(infile=None, outfile=None, *args, **kwargs):
+    # dimensions of Olympic
+    x_dim = 200
+    y_dim = 180
+    dim_value = {'x': np.arange(x_dim),
+                 'y': np.arange(y_dim)}
+    if not(infile is None):
+        indata = Dataset(infile, 'r')
+        x_dim, = indata.variables['x'].shape
+        y_dim, = indata.variables['y'].shape
+        dim_value['x'] = indata.variables['x'][:]
+        dim_value['y'] = indata.variables['y'][:]
+        indata.close()
+    outdata = Dataset(outfile, 'w')
 
-outdata.close()
+    air_temp_mean_annual = kwargs['air_temp_mean_annual']
+    air_temp_mean_july = kwargs['air_temp_mean_july']
+    precipitation = kwargs['precipitation']
+    usurf = kwargs['usurf']
+
+    dim_len = {'x': x_dim, 'y': y_dim, 'time': None}
+    for dim_name in ['x', 'y', 'time']:
+        try:
+            outdata.createDimension(dim_name, dim_len[dim_name])
+            outdata.createVariable(dim_name, np.float64, (dim_name,))
+            if not(dim_len[dim_name] is None):
+                outdata.variables[dim_name][:] = dim_value[dim_name]
+        except:
+            continue
+
+    var_name_list = ['air_temp_mean_annual',
+                     'air_temp_mean_july',
+                     'precipitation',
+                     'usurf']
+    var_value = {'air_temp_mean_annual': air_temp_mean_annual,
+                 'air_temp_mean_july': air_temp_mean_july,
+                 'precipitation': precipitation,
+                 'usurf': usurf}
+    var_unit = {'air_temp_mean_annual': 'celsius',
+                 'air_temp_mean_july': 'celsius',
+                 'precipitation': 'mm yr-1',
+                 'usurf': 'm'}
+    for var_name in var_name_list:
+        try:
+            var = outdata.variables[var_name]
+        except:
+            var = outdata.createVariable(var_name, np.float64, ('y', 'x',))
+        var[:] = var_value[var_name]
+        var.units = var_unit[var_name]
+
+    outdata.variables['usurf'].standard_name = 'surface_altitude'
+
+    outdata.close()
+
+if __name__=='__main__':
+    infile = '../bed_dem/test_dem.nc'
+    outfile = 'test_climate.nc'
+    build_constant_climate(infile=infile, outfile=outfile) 
+    build_paleo_modifier(delta_T = [100, 200], frac_P=[-100, -200])
