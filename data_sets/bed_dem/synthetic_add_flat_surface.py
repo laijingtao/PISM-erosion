c63818c18e43f229e7b95c3cc4fb1400113c41fb
laijingtao <laijingtao@gmail.com>
2017-11-21 15:17:52 -0600

    add ramp

--- data_sets/bed_dem/synthetic_add_flat_surface.py
+++ data_sets/bed_dem/synthetic_add_flat_surface.py
@@ -67,8 +67,10 @@ def add_flat_surface(infile=None, outfile=None, width=20000.0, loc='both', flat_
 
     indata.close()
     outdata.close()
-
-def add_flat_surface_left(infile=None, outfile=None, width=20000.0):
+    
+def add_ramp(infile=None, outfile=None, width=20000.0, loc='both', ramp_depth=200.0):
+    if not(loc in ['right', 'left', 'both']):
+        sys.exit('loc is invalid')
     indata = Dataset(infile, 'r')
     outdata = Dataset(outfile, 'w')
     
@@ -78,75 +80,64 @@ def add_flat_surface_left(infile=None, outfile=None, width=20000.0):
 
     z_in_var = indata.variables['topg']
     z_in = z_in_var[:]
-    z_flat = np.zeros((z_in.shape[0], int(width/grid)))
-    #z_flat[:] = z_in.min()
-    z_flat[:] = -1.0
-    z_out = np.concatenate((z_flat, z_in), axis=1)
-    z_out[np.where(z_out==z_in_var._FillValue)] = np.nan
-    z_out = np.ma.array(z_out, mask=np.isnan(z_out))
-    z_out -= z_out.min()
 
-    outdata.createDimension('x', int(ncols+width/grid))
-    outdata.createDimension('y', int(nrows))
-    x_var = outdata.createVariable('x', np.float64, ('x',))
-    y_var = outdata.createVariable('y', np.float64, ('y',))
-    z_var = outdata.createVariable('topg', np.float64, ('y', 'x',),
-                                   fill_value=z_in_var._FillValue)
+    z_ramp = np.zeros((z_in.shape[0], int(width/grid)))
+    ni, nj = z_ramp.shape
+    for j in range(nj):
+        z_ramp[:, j] = -ramp_depth*(1.0-float(j)/float(nj))
 
-    x_var[:] = np.arange(-width+grid/2.0,
-                         indata.variables['x'][:].max()+grid/2.0,
-                         grid)
-    x_var.units = 'm'
-    x_var.standard_name = 'projection_x_coordinate'
-    y_var[:] = indata.variables['y'][:]
-    y_var.units = 'm'
-    y_var.standard_name = 'projection_y_coordinate'
-    z_var[:] = z_out
-    z_var.units = 'm'
-    z_var.standard_name = 'bedrock_altitude'
+    np.random.seed()
+    z_ramp += np.random.rand(z_ramp.shape[0], z_ramp.shape[1])*5
 
-    indata.close()
-    outdata.close()
-
-def add_flat_surface_right(infile=None, outfile=None, width=20000.0):
-    indata = Dataset(infile, 'r')
-    outdata = Dataset(outfile, 'w')
+    if loc == 'left':
+        z_out = np.concatenate((z_ramp, z_in), axis=1)
+    elif loc == 'right':
+        z_out = np.concatenate((z_in, np.flip(z_ramp, 1)), axis=1)
+    elif loc == 'both':
+        z_tmp = np.concatenate((z_ramp, z_in), axis=1)
+        z_out = np.concatenate((z_tmp, np.flip(z_ramp, 1)), axis=1)
     
-    grid = abs(indata.variables['x'][1]-indata.variables['x'][0])
-    ncols = len(indata.variables['x'][:])
-    nrows = len(indata.variables['y'][:])
-
-    z_in_var = indata.variables['topg']
-    z_in = z_in_var[:]
-    z_flat = np.zeros((z_in.shape[0], int(width/grid)))
-    z_flat[:] = z_in.min()
-    z_out = np.concatenate((z_in, z_flat), axis=1)
-    z_out[np.where(z_out==z_in_var._FillValue)] = np.nan
+    fill_value = -2.0e9
+    #fill_value = z_in_var._FillValue
+    z_out[np.where(z_out==fill_value)] = np.nan
     z_out = np.ma.array(z_out, mask=np.isnan(z_out))
-    z_out -= z_out.min()
+    #z_out -= z_out.min()
 
-    outdata.createDimension('x', int(ncols+width/grid))
-    outdata.createDimension('y', int(nrows))
-    x_var = outdata.createVariable('x', np.float64, ('x',))
-    y_var = outdata.createVariable('y', np.float64, ('y',))
-    z_var = outdata.createVariable('topg', np.float64, ('y', 'x',),
-                                   fill_value=z_in_var._FillValue)
-
-    x_var[:] = np.arange(indata.variables['x'][:].min(),
-                         indata.variables['x'][:].max()+width+grid/2.0,
-                         grid)
+    if loc =='left':
+        outdata.createDimension('x', int(ncols+width/grid))
+        x_var = outdata.createVariable('x', np.float64, ('x',))
+        x_var[:] = np.arange(-width+grid/2.0,
+                             indata.variables['x'][:].max()+grid/2.0,
+                             grid)
+    elif loc == 'right':
+        outdata.createDimension('x', int(ncols+width/grid))
+        x_var = outdata.createVariable('x', np.float64, ('x',))
+        x_var[:] = np.arange(indata.variables['x'][:].min(),
+                             indata.variables['x'][:].max()+width+grid/2.0,
+                             grid)
+    elif loc == 'both':
+        outdata.createDimension('x', int(ncols+width*2/grid))
+        x_var = outdata.createVariable('x', np.float64, ('x',))
+        x_var[:] = np.arange(-width+grid/2.0,
+                             indata.variables['x'][:].max()+width+grid/2.0,
+                             grid)
     x_var.units = 'm'
     x_var.standard_name = 'projection_x_coordinate'
+
+    outdata.createDimension('y', int(nrows))
+    y_var = outdata.createVariable('y', np.float64, ('y',))
     y_var[:] = indata.variables['y'][:]
     y_var.units = 'm'
     y_var.standard_name = 'projection_y_coordinate'
+
+    z_var = outdata.createVariable('topg', np.float64, ('y', 'x',),
+                                   fill_value=fill_value)
     z_var[:] = z_out
     z_var.units = 'm'
     z_var.standard_name = 'bedrock_altitude'
 
     indata.close()
     outdata.close()
-
 
 if __name__ == '__main__':
     add_flat_surface_left('test_dem.nc', 'test_dem_tmp.nc')
